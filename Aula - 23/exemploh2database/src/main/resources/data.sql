0dcfe606d248eb764134ecd2625354e6dc292ccf
Thiago Dias <thiago_dasd@hotmail.com>
2020-04-21 17:57:32 -0300

    Aula 23

---
+++ Aula - 23/exemploh2database/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into funcionario(id_funcionario, nome, salario, rg)
+    values(null, 'Thiago Dias', 2000.89, 1231231231);
+
+insert into funcionario(id_funcionario, nome, salario, rg)
+    values(null, 'Bruno Rocha', 2500.89, 3213213211);
