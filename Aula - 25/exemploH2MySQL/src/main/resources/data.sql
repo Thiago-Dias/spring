67ef5c1fc1efd8e90505e6b597dd1ee33366abda
Thiago Dias <thiago_dasd@hotmail.com>
2020-04-26 10:44:56 -0300

    Aula 25

---
+++ Aula - 25/exemploH2MySQL/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT into cliente(id_cliente, nome, renda)
+values(1, 'thiago dias', 3000.00);
+
+INSERT into cliente(id_cliente, nome, renda)
+values(2, 'bruno rocha', 2500.00);
+
+INSERT into cliente(id_cliente, nome, renda)
+values(3, 'peterson kuhn', 2000.00);
