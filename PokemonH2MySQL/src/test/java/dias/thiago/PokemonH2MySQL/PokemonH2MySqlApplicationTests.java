76462828c7817312bf6af7fb6c204b5d838a24f3
Thiago Dias <thiago_dasd@hotmail.com>
2020-04-25 23:09:47 -0300

    Exercicio Pokemon aula 25

---
+++ PokemonH2MySQL/src/test/java/dias/thiago/PokemonH2MySQL/PokemonH2MySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dias.thiago.PokemonH2MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonH2MySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
