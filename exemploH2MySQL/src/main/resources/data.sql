a1158c499d10088b8399dd4ae5a3da356a54206b
Thiago Dias <thiago_dasd@hotmail.com>
2020-04-26 10:39:55 -0300

    Removed exemploH2MySQL directory

--- exemploH2MySQL/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT into cliente(id_cliente, nome, renda)
-values(1, 'thiago dias', 3000.00);
-
-INSERT into cliente(id_cliente, nome, renda)
-values(2, 'bruno rocha', 2500.00);
-
-INSERT into cliente(id_cliente, nome, renda)
-values(3, 'peterson kuhn', 2000.00);
--- exemploH2MySQL/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP table if exists cliente;
-
-CREATE table cliente (
-        id_cliente BIGINT AUTO_INCREMENT NOT NULL,
-        nome VARCHAR(255) NULL,
-        renda DOUBLE NULL,
-        PRIMARY KEY (id_cliente));
